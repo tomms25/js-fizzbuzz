a70b4cee0d44f96116cdadcb4077151f47d06e44
tomms25 <tommasobaccano@gmail.com>
2022-11-09 15:31:45 +0100

    Prova gnerazione numeri da 1 a 100

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+// Scrivere una programma che stampi in console i numeri da 1 a 100
+
+
+// associo l'elemento Html container a quello Js
+
+let container = document.getElementById("container");
+
+
+// Creao il programma tramite for che inserisca i numeri da 1 a 100 nella console
+
+
+for (let i = 1; i <= 100; i++){
+    
+}
